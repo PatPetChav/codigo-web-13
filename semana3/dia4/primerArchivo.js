a095bdeef5e940fb10b8e0cc4fb8521ca5cdc998
PatPetChav <paelpec@yahoo.com>
2022-02-11 01:04:00 -0300

    ejemplos js

--- semana3/dia4/primerArchivo.js
+++ semana3/dia4/primerArchivo.js
@@ -1,5 +1,5 @@
 //alert('Hola Mundo');
-const numero1=10
+/*const numero1=10
 const numero2=20
 
 let resultado=0
@@ -29,10 +29,10 @@ console.warn("Resultado:",resultado)
 //console.log("Valor:",valor)
 
 //calculador
-const valor1=prompt("Ingrese un valor1")
-const valor2=prompt("Ingrese un valor2")
+//const valor1=prompt("Ingrese un valor1")
+//const valor2=prompt("Ingrese un valor2")
 
-const operacion=prompt("Ingrese tipo operacion")
+//const operacion=prompt("Ingrese tipo operacion")
 
 let resultadoCalculadora = 0
 
@@ -58,6 +58,46 @@ if (operacion ==="+") {
 
 } else {
     console.error("Operacion no valida")
+}*/
+
+
+
+
+//algoritmo que pida dos numeros y que imprima el numero mayor
+/*const numero1=prompt("Ingrese un numero1")
+const numero2=prompt("Ingrese un numero2")
+
+if (+numero1 > +numero2) {
+    console.log("El numero mayor es:",numero1)
+} else {
+    console.log("El numero mayor es:",numero2)
+}*/
+
+
+
+// and y or
+
+/*const edadMayor=18
+const nombreMayor="Juan"
+
+if (edadMayor===18 && nombreMayor==="Juan") {
+    console.log("El usuario es Juan y tiene 18 años")
 }
 
+if (edadMayor===18  ││ nombreMayor==="Juan") {
+    console.log("El usuario es Juan o tiene 18 años")
+}*/
 
+//escribir un algoritmo que pida 3 numeros e imprima el mayor
+const numero1 = +prompt("Ingrese un numero1")
+const numero2 = +prompt("Ingrese un numero2")
+const numero3 = +prompt("Ingrese un numero3")
+
+let mensaje = "El numero mayor es numero 3"
+
+if (numero1 > numero2 && numero1 > numero3 ) {
+    mensaje="El numero mayor es numero 1"
+} else if (numero2 > numero1 && numero2 > numero3 ) {
+    mensaje="El numero mayor es numero 2"
+}
+console.log(mensaje)
