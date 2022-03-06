c340f6dc122c672e67d4115708a81bcf37ffe167
PatPetChav <paelpec@yahoo.com>
2022-03-06 19:13:42 -0300

    reto11

--- semana6/dia3/reto11/starWars.js
+++ semana6/dia3/reto11/starWars.js
@@ -1,50 +1,74 @@
-const ULR_SW_IMAGES = "https://akabab.github.io/starwars-api/api/all.json";
+const ULR_SW = "https://akabab.github.io/starwars-api/api/all.json";
 
-const containerElements = document.querySelector("#container-elements");
+const containerElements = document.querySelector("#container-elements")
+const tituloModal = document.querySelector("#modal-title")
+
+const actorName = document.querySelector("#actor-name")
+const bornLocation = document.querySelector("#born-location")
+const specie = document.querySelector("#specie")
+const hairColor = document.querySelector("#hair-color")
+const eyeColor = document.querySelector("#eye-color")
+const skinColor = document.querySelector("#skin-color")
+const heightActor = document.querySelector("#height")
+const mass = document.querySelector("#mass")
+const wiki = document.querySelector("#wiki")
 
 const obtenerData = async () => {
-  const reponse = await fetch("https://swapi.dev/api/people");
+  const reponse = await fetch(ULR_SW);
   const data = await reponse.json();
 
   //console.log(data.results)
-  renderData(data.results);
+  renderData(data);
 }
 
 const renderData = (data) => {
     //console.log("aquiiiiii",data)
-    data.map((character) => {
-        
-      let imageActor = getImageFromStarWars(character.name)
-      console.log("miac",imageActor)
- 
+    data.map((actor) => {
+     
       const html = `
         <div class="col-md-4 mb-5">
             <div class="card-body">
-                <h1 class="title">${character.name}</h1>
+                <h1 >${actor.name}</h1>
                 <div class="text-center"> 
-                      <img id="actor-image" class="actor-image" width="200" src="https://codigo.edu.pe/public/img/desarrollo-movil-fluter.jpg" alt=""/>
+                      <img id="actor-image" 
+                      onclick='obtenerDetalleActor("${actor.name}")'
+                      data-bs-toggle='modal' data-bs-target='#modalActor'
+                      class="actor-image" src=${actor.image} alt=""/>
                 </div>
             </div>
         </div>
       `
   
-        console.log("aqui", character.name)
+        //console.log("aqui", actor)
         containerElements.innerHTML += html
     })
 }
 
-const getImageFromStarWars = async (nombreActor) => {
-    const response = await fetch(ULR_SW_IMAGES);
-    const imagenes = await response.json();
-  
-      const resultado = imagenes.find((imagen) => imagen.name === nombreActor)
-      console.log("aqui",resultado)
-      return resultado.image
-   
-  
-    //console.log("aqui",actors);
-  };
+const obtenerDetalleActor = async (nombreActor) => {
+    //console.log("envia",nombreActor)
+    data.map((actor) => { 
+        //console.log("el actor",actor.name) 
+        if(actor.name === nombreActor) {
+            tituloModal.innerHTML= "este s el titulo"
+            actorName.innerHTML = actor.name
+            bornLocation.innerHTML = actor.bornLocation
+            specie.innerHTML = actor.specie
+            hairColor.innerHTML = actor.hairColor
+            eyeColor.innerHTML = actor.eyeColor
+            skinColor.innerHTML = actor.skinColor
+            heightActor.innerHTML = actor.heightActor
+            mass.innerHTML = actor.mass
+            wiki.innerHTML =  actor.wiki                      
+            return
+        }
+    })
+    
+    /*const reponse = await fetch(ULR_SW);
+    const data = await reponse.json();*/
 
-
+    
+  
+  }
 
 obtenerData()
+
